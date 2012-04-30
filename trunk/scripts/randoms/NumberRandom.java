93e0863b7069a1f1397def753c748a2384ae9dae
monkeybotmaker@gmail.com <monkeybotmaker@gmail.com@19871b03-35b5-0038-4dd0-995af353654a>
2012-04-30 09:22:09 +0000

    anti random fixed, for real this time

--- trunk/scripts/randoms/NumberRandom.java
+++ trunk/scripts/randoms/NumberRandom.java
@@ -12,24 +12,29 @@ public class NumberRandom extends Random {
 
 	@Override
 	public int loop() {
-		System.out.println("numberrandom");
 		RSInterface[] parents = interfaces.getAllParents();
 		for (RSInterface parent : parents) {
 			if (parent != null) {
 				for (GameInterface child : parent.getParentInterface()) {
 					if (child != null) {
-						if (child.getText() != null && child.getText().contains("word is") && child.getId() !=  2162882|| child.getText() != null && child.getText().contains("the word")) {
-
+						if (child.getText() != null
+								&& child.getText().contains("word is")
+								&& child.getId() != 2162882
+								&& !child
+										.getText()
+										.equals("Your password is only as safe as your computer.")
+								|| child.getText() != null
+								&& child.getText().contains("the word")) {
+							// type the random text
 							String text = child.getText();
 							String[] subString = null;
-							if(text.contains("word is")){
-							subString = child.getText().split(
-									"word is ");
-							}else if(text.contains("the word")){
-								 subString = child.getText().split(
-										"the word ");
+							if (text.contains("word is")) {
+								subString = child.getText().split("word is ");
+							} else if (text.contains("the word")) {
+								subString = child.getText().split("the word ");
 							}
 							String text2 = subString[1];
+							// send it
 							Keyboard k = new Keyboard();
 							k.sendKeys(text2);
 						}
@@ -48,7 +53,13 @@ public class NumberRandom extends Random {
 				for (GameInterface child : parent.getParentInterface()) {
 					if (child != null) {
 						if (child.getText() != null
-								&& child.getText().contains("word is") || child.getText() != null && child.getText().contains("the word")) {
+								&& child.getText().contains("word is")
+								&& child.getId() != 2162882
+								&& !child
+										.getText()
+										.equals("Your password is only as safe as your computer.")
+								|| child.getText() != null
+								&& child.getText().contains("the word")) {
 							return true;
 						}
 					}
